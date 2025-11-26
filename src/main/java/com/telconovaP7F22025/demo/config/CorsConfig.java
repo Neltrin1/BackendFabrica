f2b751773364ce81bddca48bdb279febaed25a0e
Neltrin1 <69720571+Neltrin1@users.noreply.github.com>
2025-11-26 00:50:27 -0500

    Update CorsConfig.java

--- src/main/java/com/telconovaP7F22025/demo/config/CorsConfig.java
+++ src/main/java/com/telconovaP7F22025/demo/config/CorsConfig.java
@@ -1,3 +1,5 @@
+package com.telconovaP7F22025.demo.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
